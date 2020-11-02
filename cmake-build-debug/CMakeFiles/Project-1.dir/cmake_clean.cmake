ca21ebb6cb7905aa9bc3ae0299790f63c3e465c2
treloubas76 <chris96skan@gmail.com>
2020-11-02 11:25:29 +0200

    changen name from testproject to Project-1

---
+++ cmake-build-debug/CMakeFiles/Project-1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project-1.dir/main.cpp.o"
+  "Project-1"
+  "Project-1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project-1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Project-1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Project-1.
+# This may be replaced when dependencies are built.
